0967bb3f890e8e97fec3ec2d6a4c938ec00f238d
x7airworker <jake@jakes-co.de>
2019-10-26 11:28:04 +0200

    Remove seats system

--- seats/server.lua
+++
@@ -1,9 +0,0 @@
-AddRemoteEvent("OnSeat", function(player, x, y, z, h)
-    SetPlayerLocation(player, x, y, z) 
-    SetPlayerHeading(player, h)
-    SetPlayerAnimation(player, "SIT04")
-end)
-
-AddRemoteEvent("OnSeatLeave", function(player)
-    SetPlayerAnimation(player, "STOP")
-end)
