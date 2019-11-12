8117e125cac06bc353c4d7b07f36a9ee3ad7c4ff
JanHolger <jan@bebendorf.eu>
2019-11-12 23:19:19 +0100

    Added fixed time option

--- environment/client.lua
+++ environment/client.lua
@@ -1,19 +1,15 @@
-AddRemoteEvent("OnEnvironmentExchange", function(data)
+AddRemoteEvent("TimeUpdate", function(data)
     -- add sync stuff
     local timeMode = data["time"]["mode"]
     if timeMode == nil then
         return
     end
-    local serverTime = data["time"]["serverTime"]
-    if timeMode == "realtime" then
-        SetTime(serverTime / 3600)
-        CreateTimer(function()
-            serverTime = serverTime + 10
-            if serverTime / 3600 > 24 then
-                serverTime = 0
-            end
-            SetTime(serverTime / 3600)
-        end, 10000)
+    if timeMode == "real" then
+        SetTime(data["time"]["server_time"] / 3600)
+        return
+    end
+    if timeMode == "fixed" then
+        SetTime(data["time"]["time"])
         return
     end
 end)
--- environment/server.lua
+++ environment/server.lua
@@ -1,6 +1,15 @@
 AddEvent("OnPlayerJoin", function(player)
-    local data = {}
-    data["time"] = config["time"]
-    data["time"]["serverTime"] = GetTimeSeconds()
-    CallRemoteEvent(player, "OnEnvironmentExchange", data)
+    local data = config["time"]
+    if data["time"]["mode"] == "real" then
+        data["time"]["server_time"] = GetTimeSeconds()
+    end
+    CallRemoteEvent(player, "TimeUpdate", data)
 end)
+
+if data["time"]["mode"] == "real" then
+    CreateTimer(function()
+        local data = config["time"]
+        data["time"]["server_time"] = GetTimeSeconds()
+        CallRemoteEvent(player, "TimeUpdate", data)
+    end, 60000)
+end
